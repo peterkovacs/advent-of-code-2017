d098f5e763c7a7c432dea0bafc116a3d1dbf1b8b
Peter Kovacs <peter@sanebox.com>
2017-12-04 11:34:44 -0500

    Day 2.

--- Sources/Day2/main.swift
+++ Sources/Day2/main.swift
@@ -1,7 +1,39 @@
 import Foundation
 import Lib
-//import FootlessParser
+import FootlessParser
 //import CommonCrypto
 
+let number = { Int($0)! } <^> oneOrMore( digit )
+let parser = oneOrMore( number <* zeroOrMore( whitespace ) )
 
-print("Day Day2")
+extension Collection where Element: Comparable {
+  func minmax() -> (Element, Element) {
+    var min = self.first!
+    var max = self.first!
+
+    for i in self {
+      if min > i {
+        min = i
+      }
+      if max < i {
+        max = i
+      }
+    }
+
+    return (min, max)
+  }
+}
+
+let data = STDIN.map { try! parse( parser, $0 ) }
+let part1 = data.map { $0.minmax() }.map { $1 - $0 }.reduce(0, +)
+print( part1 )
+
+let part2 = data.map { (line: [Int]) -> (Int) in
+  let pair = Array( line.lazyCombos(n: 2).filter { $0[0] % $0[1] == 0 || $0[1] % $0[0] == 0 } )
+
+  assert( pair.count == 1 )
+  assert( pair[0].count == 2 )
+
+  return (pair[0].max()! / pair[0].min()!)
+}.reduce(0, +)
+print( part2 )
