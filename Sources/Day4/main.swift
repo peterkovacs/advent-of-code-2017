b6f846dab15f298085b15894092be1b071eead6c
Peter Kovacs <peter@sanebox.com>
2017-12-04 15:28:33 -0500

    Day 4: Use a more reasonable data structure: Set

--- Sources/Day4/main.swift
+++ Sources/Day4/main.swift
@@ -7,18 +7,12 @@ let passphrase = extend <^> oneOrMore( oneOrMore( alphanumeric ) <* whitespace )
 let passphrases = STDIN.map { try! parse( passphrase, $0 ) }
 
 let part1 = passphrases.filter { p in
-  var words = Frequency<String>()
-  words.add( p )
-  
-  return words.count == p.count
+  return Set(p).count == p.count
 }
 
 print( part1.count )
 
 let part2 = passphrases.filter { p in
-  var words = Frequency<String>()
-  words.add( p.map { String($0.sorted()) } )
-
-  return words.count == p.count
+  return Set( p.map { String($0.sorted()) } ).count == p.count
 }
 print( part2.count )
