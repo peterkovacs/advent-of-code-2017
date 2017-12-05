f89732591f0ea9cd74eb277d7a4523b650b3038b
Peter Kovacs <peter@sanebox.com>
2017-12-05 08:05:42 -0500

    Day5

--- Sources/Day5/main.swift
+++ Sources/Day5/main.swift
@@ -1,7 +1,47 @@
 import Foundation
 import Lib
-//import FootlessParser
-//import CommonCrypto
 
+var jumps = STDIN.map { Int($0)! }
+var part1 = jumps
 
-print("Day Day5")
+func part1( _ jumps: [Int] ) -> Int {
+  var jumps = jumps
+  var instruction = jumps.startIndex
+  var steps = 0
+
+  while instruction < jumps.endIndex {
+    let current = instruction
+    steps += 1
+
+    defer { jumps[current] += 1 }
+
+    instruction = jumps.index( instruction, offsetBy: jumps[ instruction ], limitedBy: jumps.endIndex ) ?? jumps.endIndex
+  }
+  return steps
+}
+
+func part2( _ jumps: [Int] ) -> Int {
+  var jumps = jumps
+  var instruction = jumps.startIndex
+  var steps = 0
+  while instruction < jumps.endIndex {
+    let current = instruction
+    steps += 1
+
+    defer { 
+      switch jumps[current] {
+      case 3...:
+        jumps[current] -= 1
+      default:
+        jumps[current] += 1
+      }
+    }
+
+    instruction = jumps.index( instruction, offsetBy: jumps[ instruction ], limitedBy: jumps.endIndex ) ?? jumps.endIndex
+  }
+
+  return steps
+}
+
+print( part1( jumps ) )
+print( part2( jumps ) )
