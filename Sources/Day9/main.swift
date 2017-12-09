82b4e45d528b8e098b96a81c09579cb58a4400b4
Peter Kovacs <peter@sanebox.com>
2017-12-09 16:31:12 -0500

    Day 9
    Added a few additional utility parsers.

--- Sources/Day9/main.swift
+++ Sources/Day9/main.swift
@@ -1,7 +1,38 @@
 import Foundation
 import Lib
-//import FootlessParser
+import FootlessParser
 //import CommonCrypto
 
+let negated = char("!") *> any() *> pure(0)
+let garbageCharacter = noneOf("!>") *> pure(0)
+let garbage: Parser<Character, Int> =
+  zeroOrMore(negated <|> garbageCharacter, 0, accumulator: +).between(char("<"), char(">"))
 
-print("Day Day9")
+func part1(_ n: Int) -> Parser<Character,Int> {
+  return 
+    lazy({ n + $0 } <^>
+         optional( 
+           separated( part1(n + 1).between(char("{"), char("}")) <|>
+                        garbage,
+                      by: char(","), initial: 0, accumulator: +),
+           otherwise: 0))
+}
+
+let countGarbage: Parser<Character, Int> = 
+  zeroOrMore( (negated *> pure(0)) <|>
+              (garbageCharacter *> pure(1)), 0, accumulator: + )
+  .between(char("<"), char(">"))
+
+func part2() -> Parser<Character,Int> {
+  return 
+    lazy(
+      optional(
+        separated( part2().between(char("{"), char("}")) <|>
+                     countGarbage,
+                   by: char(","), initial: 0, accumulator: +), 
+        otherwise: 0))
+}
+
+let input = readLine( strippingNewline: true )!
+print( try! parse( part1(0), input ) )
+print( try! parse( part2(), input ) )
--- Sources/Lib/Parsers.swift
+++ Sources/Lib/Parsers.swift
@@ -9,3 +9,57 @@ public extension Parser {
     return self.between( p, p )
   }
 }
+
+extension Array {
+  func appending( _ element: Element ) -> Array<Element> {
+    var result = self
+    result.append( element )
+    return result
+  }
+}
+
+public func separated<T,A,B,O>(_ p: Parser<T,A>, by: Parser<T,B>, initial: O, accumulator: @escaping (O,A) -> O) -> Parser<T,O> {
+  return Parser { input in
+    var working = input
+    var results = initial
+    
+    results = accumulator( results, try p.parse(&working) )
+    input = working
+
+    do {
+      while true {
+        _ = try by.parse(&working)
+        results = accumulator( results, try p.parse(&working) )
+        input = working
+      }
+    } catch {
+      return results
+    }
+  }
+}
+
+public func separated<T,A,B>( _ p: Parser<T,A>, by: Parser<T,B> ) -> Parser<T,[A]> {
+  let initial = [A]()
+  return separated(p, by: by, initial: initial) { $0.appending($1) }
+}
+
+/** Apply parser once, then repeat until it fails. Returns an array of the results. */
+public func oneOrMore <T,A,O> (_ p: Parser<T,A>, _ initial: O, accumulator: @escaping (O, A) -> O ) -> Parser<T,O> {
+    return Parser { input in
+        let first = try p.parse(&input)
+        var result = accumulator( initial, first )
+        while true {
+            do {
+                let next = try p.parse(&input)
+                result = accumulator( result, next )
+            } catch {
+                return result
+            }
+        }
+    }
+}
+
+/** Repeat parser until it fails. Returns an array of the results. */
+public func zeroOrMore <T,A,O> (_ p: Parser<T,A>, _ initial: O, accumulator: @escaping (O, A) -> O ) -> Parser<T,O> {
+    return optional( oneOrMore(p, initial, accumulator: accumulator), otherwise: initial )
+}
