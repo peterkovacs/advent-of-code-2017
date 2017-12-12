c18bf3b5d850aa45857e3de59510880e666d0f58
Peter Kovacs <peter@sanebox.com>
2017-12-12 07:34:22 -0500

    Day 12.

--- Sources/Day12/main.swift
+++ Sources/Day12/main.swift
@@ -1,7 +1,50 @@
 import Foundation
 import Lib
-//import FootlessParser
+import FootlessParser
 //import CommonCrypto
 
+let parser = { a in { b in (a, b) } } <^> unsignedInteger <*> (string( " <-> " ) *> separated( unsignedInteger, by: string( ", " ) ))
+let input = Dictionary( uniqueKeysWithValues: STDIN.map{ try! parse(parser, $0) } )
 
-print("Day Day12")
+func part1(input: [Int:[Int]]) -> Int {
+  var data = Set<Int>([0])
+  var working = input[0, default: []]
+
+  while !working.isEmpty {
+    let next = working.removeFirst()
+    if data.update(with: next) == nil {
+      working.append( contentsOf: input[next, default: []] )
+    }
+  }
+  return data.count
+}
+print(part1(input: input))
+
+
+func part2(input: [Int:[Int]]) -> Int {
+  var number = 1
+  var part2 = Set<Int>([0])
+  var input = input
+  var working = input[0, default: []]
+
+  input[0] = nil
+
+  while !input.isEmpty {
+    while !working.isEmpty {
+      let next = working.removeFirst()
+      if part2.update(with: next) == nil {
+        working.append(contentsOf: input[next, default: []])
+        input[next] = nil
+      }
+    }
+
+    number += 1
+    if let first = input.first {
+      working = first.value
+      input[first.key] = nil
+    }
+  }
+
+  return number
+}
+print(part2(input: input))
