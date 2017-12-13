3f726dcfbbb99ad46071fead9cc4631627856dd2
Peter Kovacs <peter@sanebox.com>
2017-12-13 08:23:36 -0500

    Day 13.

--- Sources/Day13/main.swift
+++ Sources/Day13/main.swift
@@ -1,7 +1,43 @@
 import Foundation
 import Lib
-//import FootlessParser
+import FootlessParser
 //import CommonCrypto
 
+struct Firewall {
+  let layers: [Int:Int]
+  let numberOfLayers: Int
 
-print("Day Day13")
+  init<S>( input: S ) where S:Sequence, S.Element == (Int,Int) {
+    layers = Dictionary( uniqueKeysWithValues: input )
+    numberOfLayers = layers.keys.max() ?? 0
+  }
+
+  func severity( at picoseconds: Int, delay: Int = 0 ) -> Int? {
+    if let period = layers[picoseconds] {
+
+      if (picoseconds + delay) % (period * 2 - 2) == 0 {
+        return picoseconds * period
+      }
+    }
+
+    return nil
+  }
+
+  func canLeaveAt( delay: Int ) -> Bool {
+    if (0...numberOfLayers).index( where: { severity(at: $0, delay: delay ) != nil } ) != nil {
+      return false
+    } else { 
+      return true
+    }
+  }
+}
+
+let parser = tuple <^> integer <*> (string( ": " ) *> integer)
+let firewall = Firewall( input: STDIN.map { try! parse( parser, $0 ) } )
+
+print( "PART 1" )
+print( (0...firewall.numberOfLayers).reduce(0) { $0 + (firewall.severity(at: $1) ?? 0) } )
+
+print( "PART 2" ) 
+print( (0...).first( where: { firewall.canLeaveAt(delay: $0) } ) as Any )
+
