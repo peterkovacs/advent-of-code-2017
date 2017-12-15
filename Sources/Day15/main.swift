818a3e2b8847a91009a47630c67b85fe65b2525c
Peter Kovacs <peter@sanebox.com>
2017-12-15 07:37:52 -0500

    Day 15.

--- Sources/Day15/main.swift
+++ Sources/Day15/main.swift
@@ -3,5 +3,26 @@ import Lib
 //import FootlessParser
 //import CommonCrypto
 
+struct Generator: Sequence, IteratorProtocol {
+  let factor: Int
+  var value: Int
 
-print("Day Day15")
+  mutating func next() -> Int? {
+    value = (value * factor) % 0x7fffffff
+    return value
+  }
+}
+
+print("PART 1")
+print(zip(Generator(factor: 16807, value: 783), 
+          Generator(factor:48271, value: 325))
+        .prefix(40_000_000)
+        .filter { (a,b) in (a&0xffff) == (b&0xffff) }
+        .count)
+
+print("PART 2")
+print(zip(Generator(factor: 16807, value: 783).lazy.filter{ $0 % 4 == 0 },
+          Generator(factor:48271, value: 325).lazy.filter{ $0 % 8 == 0 })
+        .prefix(5_000_000)
+        .filter { (a,b) in (a&0xffff) == (b&0xffff) }
+        .count)
